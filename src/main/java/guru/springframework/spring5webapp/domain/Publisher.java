ff3e74f07a82f6f7d7e548d1f617ea0384762bdf
bargotan <bargotan@interia.pl>
2020-05-12 15:11:45 +0200

    org.thymeleaf.exceptions.TemplateInputException: Error resolving template [/books/list], template might not exist or might not be accessible by any of the configured Template Resolvers

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -120,7 +120,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-//                ", books=" + books +
                 '}';
     }
 }
